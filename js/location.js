34b10d76e7e4851da815686b34b4b0f7a14a6c9b
Logan Rogers <logan.s.rogers@biola.edu>
2016-07-05 14:18:11 -0700

    restaurant info loading on location.html

--- js/location.js
+++ js/location.js
@@ -1,5 +1,5 @@
 var selectedRest = '';
-var name = document.getElementById('loc_name');
+var restName = document.getElementById('loc_name');
 var address = document.getElementById('loc_address');
 var phone = document.getElementById('loc_phone');
 var type = document.getElementById('loc_type');
@@ -15,11 +15,19 @@ function checkLocalStorage() {
     console.log('restaurant found');
   }
 };
-checkLocalStorage();
 
 function loadInfo() {
   // to load restaurant information into the DOM.
   if (selectedRest) {
-    
+    restName.textContent = selectedRest.name;
+    address.textContent = selectedRest.address;
+    phone.textContent = selectedRest.phone;
+    type.textContent = selectedRest.type;
+    rating.textContent = selectedRest.reviews[0].rating;
+    cost.textContent = selectedRest.reviews[0].cost;
+    gfc.textContent = selectedRest.reviews[0].code;
   }
 };
+
+checkLocalStorage();
+loadInfo();
