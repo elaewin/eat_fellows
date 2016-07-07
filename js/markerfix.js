87162dfe612a897ee9e79e7e9ea6e3b7f1e6a663
David Smith <dbsmith.dbs83@gmail.com>
2016-07-06 23:25:32 -0700

    spiderfier made methodical, not random

--- js/markerfix.js
+++ js/markerfix.js
@@ -1,21 +1,40 @@
 function randoSpider(listOfMarkers) {
 // used to offset lat/lng on markers w/ exact same location
+  var offsets = [[0.00015, 0],[0, 0.00015],[-0.00015, 0],[0, -0.00015],[0.00015, 0.00015],[-0.00015,0.00015],[-0.00015, -0.00015],[0.00015,-0.00015]];
   var lats = [];
   var lngs = [];
+  var tmp = [];
+  sameLocMarkerIndices = [];
   for (var i = 0; i < listOfMarkers.length; i++) {
     lats.push(listOfMarkers[i].getPosition().lat());
     lngs.push(listOfMarkers[i].getPosition().lng());
   }
   for (var j = 0; j < listOfMarkers.length; j++) {
+    tmp = [];
     for (var k = 0; k < listOfMarkers.length; k++) {
       if (listOfMarkers[j].getPosition().toString() === listOfMarkers[k].getPosition().toString() && j != k) {
-        var point = new google.maps.LatLng(listOfMarkers[j].getPosition().lat() + getRandoOffset(.00003, .00016), listOfMarkers[j].getPosition().lng() + getRandoOffset(.00003, .00016));
-        listOfMarkers[j].setPosition(point);
+        tmp.push(k);
       }
+    }
+    sameLocMarkerIndices.push(tmp);
+  }
+  var x, y;
+  for (var k = 0; k < sameLocMarkerIndices.length; k++){
+    for (var l = 0; l < sameLocMarkerIndices[k].length; l++) {
+      x = listOfMarkers[sameLocMarkerIndices[k][l]].getPosition().lat();
+      y = listOfMarkers[sameLocMarkerIndices[k][l]].getPosition().lng();
+      x += offsets[l][0];
+      y += offsets[l][1];
+      var point = new google.maps.LatLng(x,y);
+      listOfMarkers[l].setPosition(point);
     }
   }
   return listOfMarkers;
 }
+var point = new google.maps.LatLng(listOfMarkers[j].getPosition().lat() + offsets[l][0], listOfMarkers[j].getPosition().lng() + offsets[l][1]);
+
+var point = new google.maps.LatLng(listOfMarkers[j].getPosition().lat() + getRandoOffset(.00003, .00016), listOfMarkers[j].getPosition().lng() + getRandoOffset(.00003, .00016));
+listOfMarkers[j].setPosition(point);
 
 //gets random number in range positive OR negative
 function getRandoOffset(min, max) {
